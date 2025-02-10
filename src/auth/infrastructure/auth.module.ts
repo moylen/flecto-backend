23a35293bf3108788458ccfa09996603af015a6f
Moylen <moylenrh@gmail.com>
2025-02-10 13:11:55 +0700

    refactor modules structure

--- src/auth/infrastructure/auth.module.ts
+++ src/auth/infrastructure/auth.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../../user/infrastructure/user.module';
-import { UserService } from '../../user/domain/services/user.service';
 import { HashService } from '../../common/domain/services/hash.service';
 import { DatabaseModule } from '../../database/infrastructure/database.module';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from '../domain/services/auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UserTokenService } from '../../user/domain/services/user-token.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
-import { PrismaService } from '../../database/infrastructure/service/prisma.service';
 
 @Module({
     imports: [
@@ -17,14 +14,14 @@ import { PrismaService } from '../../database/infrastructure/service/prisma.serv
         UserModule,
         JwtModule.registerAsync({
             imports: [ConfigModule],
+            inject: [ConfigService],
             useFactory: async (configService: ConfigService) => ({
                 secret: configService.get<string>('ACCESS_SECRET'),
                 signOptions: { expiresIn: configService.get<string>('ACCESS_TOKEN_EXP') },
             }),
-            inject: [ConfigService],
         }),
     ],
     controllers: [AuthController],
-    providers: [UserService, HashService, AuthService, UserTokenService, JwtStrategy, PrismaService],
+    providers: [HashService, AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/user/infrastructure/user.module.ts
+++ src/user/infrastructure/user.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserService } from '../domain/services/user.service';
 import { DatabaseModule } from '../../database/infrastructure/database.module';
 import { UserTokenService } from '../domain/services/user-token.service';
-import { PrismaService } from '../../database/infrastructure/service/prisma.service';
 import { HashService } from '../../common/domain/services/hash.service';
 import { UserController } from './controllers/user.controller';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [UserController],
-    providers: [UserService, UserTokenService, PrismaService, HashService],
+    providers: [UserService, UserTokenService, HashService],
+    exports: [UserService, UserTokenService],
 })
 export class UserModule {}
