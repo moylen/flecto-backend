953a16d3f642383337e9272d766bde651624d1d2
Moylen <moylenrh@gmail.com>
2025-03-07 19:07:07 +0700

    update cors port

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     // Config
     const configService = app.get(ConfigService);
     app.enableCors({
-        origin: ['http://localhost:5174'],
+        origin: ['http://localhost:5173'],
         methods: 'GET,POST,PUT,DELETE',
         allowedHeaders: 'Content-Type, Authorization',
         credentials: true,
