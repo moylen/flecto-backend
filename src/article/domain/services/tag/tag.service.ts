fc4e58b961ce3abd22ffda02752a4af3d39f238f
Moylen <moylenrh@gmail.com>
2025-04-14 18:15:17 +0700

    update tag autocomplete

--- src/article/domain/services/tag/tag.service.ts
+++ src/article/domain/services/tag/tag.service.ts
@@ -12,6 +12,7 @@ export class TagService {
             where: {
                 title: {
                     contains: dto.query,
+                    mode: 'insensitive',
                 },
             },
             ...RepositoryHelper.applyPagination(dto),
