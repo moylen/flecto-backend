dc3259e73878dc76c14d71f2c5e59a42868b33f7
Moylen <moylenrh@gmail.com>
2025-04-14 17:56:56 +0700

    fix pagination dto

--- src/common/domain/dtos/pagination.dto.ts
+++ src/common/domain/dtos/pagination.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
 export class PaginationDto {
     @ApiProperty({ required: false })
     @IsOptional()
+    @Type(() => Number)
     @IsNumber()
     page?: number;
 
     @ApiProperty({ required: false })
     @IsOptional()
+    @Type(() => Number)
     @IsNumber()
     pageSize?: number;
 }
