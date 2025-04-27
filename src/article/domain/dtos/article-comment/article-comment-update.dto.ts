5d643f92d1baefcd078f2e8b3acfed7dd85073ed
Moylen <moylenrh@gmail.com>
2025-04-27 22:53:04 +0700

    fix article comment create dto

--- src/article/domain/dtos/article-comment/article-comment-update.dto.ts
+++ src/article/domain/dtos/article-comment/article-comment-update.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ArticleCommentUpdateDto {
     @ApiProperty()
     @IsNotEmpty()
-    @IsNumber()
+    @IsString()
     content: string;
 }
