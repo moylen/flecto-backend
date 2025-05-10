447ae5ce693fe8a8ee49bc1a76bdae5d44ec6f19
Moylen <moylenrh@gmail.com>
2025-05-10 15:17:12 +0700

    fix chat message search dto

--- src/chat/domain/dtos/chat-message-search.dto.ts
+++ src/chat/domain/dtos/chat-message-search.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { PaginationDto } from '../../../common/domain/dtos/pagination.dto';
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class ChatMessageSearchDto extends PaginationDto {
     @ApiProperty()
+    @Type(() => Number)
     @IsNotEmpty()
     @IsNumber()
     receiverId: number;
