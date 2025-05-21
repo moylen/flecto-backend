c89a625203e04d775b0ae5aa4faa8fee39d00b2c
Moylen <moylenrh@gmail.com>
2025-05-21 18:18:51 +0700

    fix tag naming

--- src/article/domain/dtos/tag/tag-save.dto.ts
+++ src/article/domain/dtos/tag/tag-save.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class TagSaveDto {
     @ApiProperty()
     @IsNotEmpty()
     @IsString()
+    @Transform(({ value }) => value.toLowerCase().trim())
     title: string;
 }
